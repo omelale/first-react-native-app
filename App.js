bd3fdd2f9718193ff1fc9e399108a0cbf7e51767
Ajten <ajtencani@gmail.com>
2021-12-25 16:27:02 +0100

    css touches to make prettier modal and buttons

--- App.js
+++ App.js
@@ -1,39 +1,52 @@
 import React, {useState} from "react";
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {Button, FlatList, StyleSheet, Text, View} from 'react-native';
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
     const [goals, setGoals] = useState([]);
+    const [modalState, setModalState] = useState(false);
     const addGoal = (goalTitle, deleteInputField) => {
         setGoals((stateOfGoals) => [...stateOfGoals, {key: Math.random().toString(), value: goalTitle}]);
         deleteInputField('');
+        closeModal();
+    }
+    const openModal = () => {
+        setModalState(true);
+    }
+    const closeModal = () => {
+        setModalState(false);
     }
     const deleteAllGoals = () => {
         setGoals([]);
+        closeModal();
     }
     const removeSingleGoal = goalId => {
         setGoals((stateOfGoals) => {
             return stateOfGoals.filter((goal) => {
-               return goal.key !== goalId;
+                return goal.key !== goalId;
             });
         });
     }
-    return (
-        <View style={styles.mainContainer}>
-            <View>
-                <Text style={styles.mainTitle}>This is the ToDo app!</Text>
-            </View>
-            <GoalInput onAddGoal={addGoal} onDeleteGoal={deleteAllGoals}/>
-            <FlatList data={goals}
-                      renderItem={itemData => <GoalItem id={itemData.item.key} deleteSingleGoal={removeSingleGoal}
-                                                        title={itemData.item.value}/>}/>
-        </View>);
+    return (<View style={styles.mainContainer}>
+        <View style={styles.secondContainer}>
+            <Text style={styles.mainTitle}>This is the ToDo app!</Text>
+
+            <Button title='Add new goal' onPress={() => {
+                openModal(true)
+            }}/>
+        </View>
+        <GoalInput opened={modalState} onAddGoal={addGoal} closeM={closeModal} onDeleteGoal={deleteAllGoals}/>
+        <FlatList data={goals}
+                  renderItem={itemData => <GoalItem id={itemData.item.key} deleteSingleGoal={removeSingleGoal}
+                                                    title={itemData.item.value}/>}/>
+    </View>);
 }
 const styles = StyleSheet.create({
     'mainContainer': {
-        padding: 30
-    }, 'mainTitle': {
+        padding: 30,
+        paddingTop:70
+    }, 'secondContainer': {marginBottom: 20}, 'mainTitle': {
         fontSize: 30, fontWeight: "700", alignItems: 'center', marginBottom: 20,
     },
 });
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,43 +1,59 @@
 import React, {useState} from "react";
-import {Button, TextInput, View, StyleSheet} from "react-native";
+import {Button, Modal, StyleSheet, TextInput, View} from "react-native";
 
 const GoalInput = (props) => {
     const [userInput, setUserInput] = useState('');
     const handleUserInput = (textUserInput) => {
         setUserInput(textUserInput);
     }
-    return <View>
+    return (<Modal visible={props.opened} animationType='slide'>
+
         <View style={styles.inputDiv}>
             <TextInput placeholder={'write a task'}
                        style={styles.textinput}
                        value={userInput}
                        onChangeText={handleUserInput}
             />
-            <Button
-                title='Add' style={styles.btn}
-                onPress={props.onAddGoal.bind(this, userInput, setUserInput)}
-            />
+
+            <View style={styles.inlineButtons}>
+                <View style={styles.button}><Button
+                    title='Add' style={styles.btn}
+                    onPress={props.onAddGoal.bind(this, userInput, setUserInput)}
+                /></View>
+                <View style={styles.button}><Button
+                    title='Cancel'
+                    color='grey'
+                    onPress={props.closeM}
+                /></View>
+            </View>
         </View>
         <View>
             <Button
-                title='Delete All'
+                title='Delete all tasks'
                 style={styles.deleteBtn}
                 color='red'
                 onPress={props.onDeleteGoal}
             />
         </View>
-    </View>
+    </Modal>)
 }
 
 const styles = StyleSheet.create({
     'inputDiv': {
-        flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+        flex: 1, justifyContent: 'center', alignItems: 'center'
     }, 'textinput': {
-        borderWidth: 3, borderColor: 'red', padding: 10, width: '80%'
-    }, 'btn': {
+        borderWidth: 3, borderColor: 'red', padding: 10, width: '80%', marginBottom: 10
+    }, 'button' : {
+      width:'30%'
+    },
+    'btn': {
         padding: 40, height: 40
     }, 'deleteBtn': {
         padding: 40, height: 40
+    }, 'closeBtn': {
+        padding: 40, height: 40
+    }, 'inlineButtons': {
+        width:'60%', flexDirection:'row', alignItems: "center", justifyContent:'space-between'
     }
 })
 
